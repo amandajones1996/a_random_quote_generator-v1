0006c549d5ccacca507b7cc550ba4eab40ac0a08
amandajones1996 <jonesamanda996@gmail.com>
2023-09-09 12:25:21 -0400

    getRandomQuote and printQuote function set up

--- js/script.js
+++ js/script.js
@@ -10,20 +10,75 @@ project 1 - A Random Quote Generator
 /*** 
  * `quotes` array 
 ***/
-
+const quotes = [
+  {
+    quote: "",
+    source: "",
+    citation: "",
+    year: ""
+  },
+  {
+    quote: "",
+    source: "",
+    citation: "",
+    year: ""
+  },
+  {
+    quote: "",
+    source: "",
+    citation: "",
+    year: ""
+  },
+  {
+    quote: "",
+    source: "",
+    citation: "",
+    year: ""
+  },
+  {
+    quote: "",
+    source: "",
+    citation: "",
+    year: ""
+  },
+];
 
 
 /***
  * `getRandomQuote` function
 ***/
-
-
-
+// console.log(quotes.length)
+function getRandomQuote(){
+  const randNum = Math.floor(Math.random() * quotes.length);
+  console.log(randNum)
+  const randQuote = quotes[randNum]
+  console.log(randQuote)
+  return randQuote
+};
+getRandomQuote()
 /***
  * `printQuote` function
 ***/
 
+function printQuote(){
+  const getquote = getRandomQuote();
 
+  const html = 
+  `<p class="quote">${getquote.quote}</p>
+  <p class="source">${getquote.source}`;
+  
+  if(getquote.citation){
+      html += `<span class="citation">${getquote.citation}</span>`
+  }
+
+  if(getquote.year){
+    html += `<span class="year">${getquote.year}</span>`
+  }
+
+  html += `</p>`
+  
+  document.getElementById('quote-box').innerHTML = html; 
+}
 
 /***
  * click event listener for the print quote button
